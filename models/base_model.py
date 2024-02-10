12fbd3cbe278e501449b55c55a6729116ef41724
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2024-02-10 04:14:43 -0800

    ahmed

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,29 @@
 import uuid
 from datetime import datetime
+from models.engine import storage
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *argc, **kwargs):
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+    
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """Return dictionary of BaseModel instance."""
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
-
+        r_dict = self.__dict__.copy()
+        r_dict["created_at"] = self.created_at.isoformat()
+        r_dict["updated_at"] = self.updated_at.isoformat()
+        r_dict["__class__"] = self.__class__.__name__
+        return r_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Defines the FileStorage"""
+import json
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Return the dictionary __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Set in __objects obj with key"""
+        obj_name = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(obj_name, obj.id)] = obj
+
+    def save(self):
+        """Serialize __objects to the JSON file __file_path"""
+        o_dict = FileStorage.__objects
+        obj_dict = {obj: o_dict[obj].to_dict() for obj in o_dict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        try:
+            with open(FileStorage.__file_path, 'r') as f:
+                objdict = json.load(f)
+                for key, value in objdict.items():
+                    class_name, obj_id = key.split('.')
+                    cls = globals()[class_name]
+                    obj_instance = cls(**value)
+                    self.new(obj_instance)
+        except FileNotFoundError:
+            pass
