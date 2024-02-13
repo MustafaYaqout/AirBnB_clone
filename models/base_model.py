8278b10b5f49baf2730f79a4d704afd548f9d6fa
MustafaYaqout <mustafa.yaqoutt@gmail.com>
2024-02-13 22:08:15 +0200

    Mu

--- models/base_model.py
+++ models/base_model.py
@@ -27,8 +27,3 @@ class BaseModel:
         r_dict["updated_at"] = self.updated_at.isoformat()
         r_dict["__class__"] = self.__class__.__name__
         return r_dict
-
-    def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
