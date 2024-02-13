34c593f6eca1969c5bfafdb8f5353e7b1824ced0
MustafaYaqout <mustafa.yaqoutt@gmail.com>
2024-02-13 15:18:45 +0200

    root

--- console.py
+++ console.py
@@ -2,10 +2,6 @@
 """Console module"""
 
 import cmd
-from models import storage
-from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
-
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter"""
@@ -25,58 +21,7 @@ class HBNBCommand(cmd.Cmd):
         """Do nothint line"""
         pass
     
-    def do_create(self, arg):
-        """Creates a new instance of BaseModel,
-        saves it (to the JSON file) and prints the id"""
-        if not arg:
-            print("** class name missing **")
-        elif arg not in self.classes :
-            print("** class doesn't exist **")
-        else:
-            new = eval(arg)()
-            new.save()
-            print(new.id)
-            
-    def do_show(self, arg):
-        """show a new instance of BaseModel,
-        saves it (to the JSON file) """
-        if not arg:
-            print("** class name missing **")
-        elif arg.split()[0] not in self.classes :
-            print("** class doesn't exist **")
-        elif len(arg.split()) < 2 :
-            print("** instance id missing **")
-        else:
-            key = arg.split()[0] + ' ' + arg.split()[1]
-            if key not in storage.all():
-                print("** no instance found **")
-            else:
-                print(storage.all()[key])
-                
-    def do_destroy(self, arg):
-        """destroy all instance of BaseModel,
-        saves it (to the JSON file) """
-        if not arg:
-            print("** class name missing **")
-        elif arg.split()[0] not in self.classes :
-            print("** class doesn't exist **")
-        elif len(arg.split()) < 2 :
-            print("** instance id missing **")
-        else:
-            key = arg.split()[0] + ' ' + arg.split()[1]
-            if key not in storage.all():
-                print("** no instance found **")
-            else:
-                del storage.all()[key]
-                storage.save()
-                
-    def do_all(self, arg):
-        """Prints all string representation of all instances
-            based or not on the class name"""
-        if not arg:
-            print([str(value) for key, value in storage.all().items()])
-        elif arg not in self.classes:
-            print("** class doesn't exist **")
+
             
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
